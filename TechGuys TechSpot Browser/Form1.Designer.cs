fe10321807289dddff80c06908e7dd8440cb98e2
TechGuysTechSpot <62471032+TechGuysTechSpot@users.noreply.github.com>
2020-03-21 08:33:55 -0500

    Add files via upload

---
+++ TechGuys TechSpot Browser/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace TechGuys_TechSpot_Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radStatusStrip1 = new Telerik.WinControls.UI.RadStatusStrip();
+            this.radWaitingBarElement1 = new Telerik.WinControls.UI.RadWaitingBarElement();
+            this.radLabelElement1 = new Telerik.WinControls.UI.RadLabelElement();
+            this.commandBarSeparator1 = new Telerik.WinControls.UI.CommandBarSeparator();
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.radButton5 = new Telerik.WinControls.UI.RadButton();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.radMenu2 = new Telerik.WinControls.UI.RadMenu();
+            this.radMenuItem8 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radMenu1 = new Telerik.WinControls.UI.RadMenu();
+            this.radMenuItem1 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem7 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem2 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem3 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem4 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem5 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem6 = new Telerik.WinControls.UI.RadMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.radTitleBar1 = new Telerik.WinControls.UI.RadTitleBar();
+            this.office2007BlackTheme1 = new Telerik.WinControls.Themes.Office2007BlackTheme();
+            this.windows7Theme1 = new Telerik.WinControls.Themes.Windows7Theme();
+            this.aquaTheme1 = new Telerik.WinControls.Themes.AquaTheme();
+            ((System.ComponentModel.ISupportInitialize)(this.radStatusStrip1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.radPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTitleBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radStatusStrip1
+            // 
+            this.radStatusStrip1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radWaitingBarElement1,
+            this.radLabelElement1,
+            this.commandBarSeparator1});
+            this.radStatusStrip1.Location = new System.Drawing.Point(0, 640);
+            this.radStatusStrip1.Name = "radStatusStrip1";
+            this.radStatusStrip1.Size = new System.Drawing.Size(968, 26);
+            this.radStatusStrip1.TabIndex = 3;
+            this.radStatusStrip1.ThemeName = "ControlDefault";
+            // 
+            // radWaitingBarElement1
+            // 
+            this.radWaitingBarElement1.AutoSize = false;
+            this.radWaitingBarElement1.Bounds = new System.Drawing.Rectangle(0, 0, 66, 18);
+            this.radWaitingBarElement1.Name = "radWaitingBarElement1";
+            this.radStatusStrip1.SetSpring(this.radWaitingBarElement1, false);
+            this.radWaitingBarElement1.Text = "";
+            // 
+            // radLabelElement1
+            // 
+            this.radLabelElement1.Name = "radLabelElement1";
+            this.radStatusStrip1.SetSpring(this.radLabelElement1, false);
+            this.radLabelElement1.Text = "Loading Website";
+            this.radLabelElement1.TextWrap = true;
+            // 
+            // commandBarSeparator1
+            // 
+            this.commandBarSeparator1.Name = "commandBarSeparator1";
+            this.radStatusStrip1.SetSpring(this.commandBarSeparator1, false);
+            this.commandBarSeparator1.VisibleInOverflowMenu = false;
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.BackgroundImage = global::TechGuys_TechSpot_Browser.Properties.Resources.blue_background_023;
+            this.radPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radPanel1.Controls.Add(this.radButton5);
+            this.radPanel1.Controls.Add(this.radButton4);
+            this.radPanel1.Controls.Add(this.radButton3);
+            this.radPanel1.Controls.Add(this.radMenu2);
+            this.radPanel1.Controls.Add(this.radTextBox2);
+            this.radPanel1.Controls.Add(this.radTextBox1);
+            this.radPanel1.Controls.Add(this.radButton2);
+            this.radPanel1.Controls.Add(this.radButton1);
+            this.radPanel1.Controls.Add(this.radMenu1);
+            this.radPanel1.Location = new System.Drawing.Point(15, 37);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(941, 102);
+            this.radPanel1.TabIndex = 1;
+            // 
+            // radButton5
+            // 
+            this.radButton5.Location = new System.Drawing.Point(644, 56);
+            this.radButton5.Name = "radButton5";
+            this.radButton5.Size = new System.Drawing.Size(29, 24);
+            this.radButton5.TabIndex = 4;
+            this.radButton5.Text = "🔍 ";
+            this.radButton5.ThemeName = "ControlDefault";
+            this.radButton5.Click += new System.EventHandler(this.radButton5_Click);
+            // 
+            // radButton4
+            // 
+            this.radButton4.Location = new System.Drawing.Point(894, 56);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(31, 24);
+            this.radButton4.TabIndex = 7;
+            this.radButton4.Text = " 🔍";
+            this.radButton4.ThemeName = "ControlDefault";
+            this.radButton4.Click += new System.EventHandler(this.radButton4_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(679, 56);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(28, 24);
+            this.radButton3.TabIndex = 6;
+            this.radButton3.Text = "⟳ ";
+            this.radButton3.ThemeName = "ControlDefault";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // radMenu2
+            // 
+            this.radMenu2.BackColor = System.Drawing.Color.SteelBlue;
+            this.radMenu2.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radMenuItem8});
+            this.radMenu2.Location = new System.Drawing.Point(0, 24);
+            this.radMenu2.Name = "radMenu2";
+            this.radMenu2.Size = new System.Drawing.Size(941, 20);
+            this.radMenu2.TabIndex = 5;
+            // 
+            // radMenuItem8
+            // 
+            this.radMenuItem8.Name = "radMenuItem8";
+            this.radMenuItem8.Text = " ";
+            // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radTextBox2.Location = new System.Drawing.Point(713, 58);
+            this.radTextBox2.Name = "radTextBox2";
+            this.radTextBox2.Size = new System.Drawing.Size(215, 20);
+            this.radTextBox2.TabIndex = 4;
+            this.radTextBox2.Text = "Search Google";
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(96, 58);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.Size = new System.Drawing.Size(604, 20);
+            this.radTextBox1.TabIndex = 3;
+            this.radTextBox1.Text = "https://google.com";
+            this.radTextBox1.ThemeName = "Office2007Black";
+            // 
+            // radButton2
+            // 
+            this.radButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radButton2.EnableGestures = false;
+            this.radButton2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radButton2.ImageAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radButton2.Location = new System.Drawing.Point(18, 54);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(31, 31);
+            this.radButton2.TabIndex = 3;
+            this.radButton2.Text = "←";
+            this.radButton2.ThemeName = "Aqua";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton1
+            // 
+            this.radButton1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radButton1.Location = new System.Drawing.Point(55, 54);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(35, 31);
+            this.radButton1.TabIndex = 1;
+            this.radButton1.Text = "→";
+            this.radButton1.ThemeName = "Aqua";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radMenu1
+            // 
+            this.radMenu1.BackColor = System.Drawing.Color.SteelBlue;
+            this.radMenu1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radMenu1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radMenuItem1,
+            this.radMenuItem2,
+            this.radMenuItem3,
+            this.radMenuItem4,
+            this.radMenuItem5});
+            this.radMenu1.Location = new System.Drawing.Point(0, 0);
+            this.radMenu1.Name = "radMenu1";
+            this.radMenu1.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
+            this.radMenu1.Size = new System.Drawing.Size(941, 24);
+            this.radMenu1.TabIndex = 0;
+            this.radMenu1.ThemeName = "ControlDefault";
+            // 
+            // radMenuItem1
+            // 
+            this.radMenuItem1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.radMenuItem1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radMenuItem7});
+            this.radMenuItem1.Name = "radMenuItem1";
+            this.radMenuItem1.Text = "File";
+            // 
+            // radMenuItem7
+            // 
+            this.radMenuItem7.Name = "radMenuItem7";
+            this.radMenuItem7.Text = "Exit";
+            this.radMenuItem7.Click += new System.EventHandler(this.radMenuItem7_Click_1);
+            // 
+            // radMenuItem2
+            // 
+            this.radMenuItem2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.radMenuItem2.Name = "radMenuItem2";
+            this.radMenuItem2.Text = "Edit";
+            // 
+            // radMenuItem3
+            // 
+            this.radMenuItem3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.radMenuItem3.Name = "radMenuItem3";
+            this.radMenuItem3.Text = "View";
+            // 
+            // radMenuItem4
+            // 
+            this.radMenuItem4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.radMenuItem4.Name = "radMenuItem4";
+            this.radMenuItem4.Text = "Help";
+            // 
+            // radMenuItem5
+            // 
+            this.radMenuItem5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.radMenuItem5.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radMenuItem6});
+            this.radMenuItem5.Name = "radMenuItem5";
+            this.radMenuItem5.Text = "About";
+            // 
+            // radMenuItem6
+            // 
+            this.radMenuItem6.Name = "radMenuItem6";
+            this.radMenuItem6.Text = "About TechGuys TechSpot Browser";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 142);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(941, 492);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Url = new System.Uri("https://google.com", System.UriKind.Absolute);
+            // 
+            // radTitleBar1
+            // 
+            this.radTitleBar1.ImageIcon = ((System.Drawing.Icon)(resources.GetObject("radTitleBar1.ImageIcon")));
+            this.radTitleBar1.Location = new System.Drawing.Point(0, -1);
+            this.radTitleBar1.Name = "radTitleBar1";
+            this.radTitleBar1.Size = new System.Drawing.Size(969, 32);
+            this.radTitleBar1.TabIndex = 0;
+            this.radTitleBar1.TabStop = false;
+            this.radTitleBar1.Text = "TechGuys TechSpot Browser";
+            this.radTitleBar1.ThemeName = "ControlDefault";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TechGuys_TechSpot_Browser.Properties.Resources.light_blue_gradient_abstract_background_8087_1850;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(968, 666);
+            this.Controls.Add(this.radStatusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.radPanel1);
+            this.Controls.Add(this.radTitleBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TechGuys TechSpot Browser";
+            ((System.ComponentModel.ISupportInitialize)(this.radStatusStrip1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.radPanel1.ResumeLayout(false);
+            this.radPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTitleBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem1;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem2;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem3;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem4;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem5;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem6;
+        private Telerik.WinControls.UI.RadTextBox radTextBox2;
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadMenu radMenu1;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem7;
+        private Telerik.WinControls.UI.RadMenu radMenu2;
+        private Telerik.WinControls.UI.RadStatusStrip radStatusStrip1;
+        private Telerik.WinControls.UI.RadWaitingBarElement radWaitingBarElement1;
+        private Telerik.WinControls.UI.RadLabelElement radLabelElement1;
+        private Telerik.WinControls.UI.CommandBarSeparator commandBarSeparator1;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem8;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private Telerik.WinControls.UI.RadButton radButton5;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private Telerik.WinControls.UI.RadTitleBar radTitleBar1;
+        private Telerik.WinControls.Themes.Office2007BlackTheme office2007BlackTheme1;
+        private Telerik.WinControls.Themes.Windows7Theme windows7Theme1;
+        private Telerik.WinControls.Themes.AquaTheme aquaTheme1;
+
+    }
+}
+
---
+++ TechGuys TechSpot Browser/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TechGuys_TechSpot_Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radMenuItem6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radMenuItem7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radMenuItem7_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void radButton4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void radButton5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(radTextBox1.Text);
+        }
+    }
+}
